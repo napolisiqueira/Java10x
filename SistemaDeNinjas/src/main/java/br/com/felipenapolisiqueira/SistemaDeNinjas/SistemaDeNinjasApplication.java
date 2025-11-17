1ae24e0447956d9b765821931c4850a4925db73e
napolisiqueira <imfelipenapoli@gmail.com>
2025-11-16 22:29:44 -0300

    Começando o primeiro projeto do curso Batismo de Java com Spring Boot

---
+++ SistemaDeNinjas/src/main/java/br/com/felipenapolisiqueira/SistemaDeNinjas/SistemaDeNinjasApplication.java
@@ -0,0 +1,13 @@
+package br.com.felipenapolisiqueira.SistemaDeNinjas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeNinjasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeNinjasApplication.class, args);
+	}
+
+}
---
+++ SistemaDeNinjas/src/test/java/br/com/felipenapolisiqueira/SistemaDeNinjas/SistemaDeNinjasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.felipenapolisiqueira.SistemaDeNinjas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeNinjasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
